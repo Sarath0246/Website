a6719019cb8a2cf48d4150850263a695192e6b38
Sarath0246 <112571705+Sarath0246@users.noreply.github.com>
2024-06-03 15:14:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end(' hiii how r u,\n');
+    response.end(' hiii howr r u,\n');
 }) .listen(3000, () => {
     console.log('Server running at http://localhost:8000/');
 });
