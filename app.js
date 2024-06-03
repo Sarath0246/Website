0155c013e920462e6405e50db57e36c59c795428
Ubuntu <ubuntu@ip-172-31-36-59.ap-south-1.compute.internal>
2024-06-03 07:23:50 +0000

    commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end(' hi how r u,\n');
+}) .listen(3000, () => {
+    console.log('Server running at http://localhost:8000/');
+});
