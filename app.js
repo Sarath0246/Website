ffec7bcb447e8aa4c6e05996edbe5e545efb2de4
Sarath0246 <112571705+Sarath0246@users.noreply.github.com>
2024-06-10 10:15:53 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end(' hi -hggggowr r u,\n');
-}) .listen(3000, () => {
-    console.log('Server running at http://localhost:8000/');
-});
